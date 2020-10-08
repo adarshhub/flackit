b435535b79eb1fb713b25f88391b2bc249619fb5
adarshhub <adarshkmr646@gmail.com>
2020-10-08 12:23:59 +0530

    Changed

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import requests, json
-from flask import Flask
+from flask import Flask, request
 from os import environ
 
 app = Flask(__name__)
 
-MY_TOKEN = environ['MY_TOKEN']
-BOT_TOKEN = environ['BOT_TOKEN']
+# MY_TOKEN = environ['MY_TOKEN']
+# BOT_TOKEN = environ['BOT_TOKEN']
+MY_TOKEN = 'xoxp-1419442495793-1403724219829-1405794636342-a85c786b064214c7ba726ec5ea57bcdb'
+BOT_TOKEN = 'xoxb-1419442495793-1413075542628-mKalOzt7rrWxCw8fwuatkSlB'
 
 TOKEN_IN_USE = BOT_TOKEN
 
@@ -37,8 +39,13 @@ def msgToChannel(channelId):
     print(response)
     return response.json()
 
+@app.route('/listen', methods=['POST'])
+def listen():
+    incoming = request.get_json()
+    return json.dumps({'challenge': incoming['challenge']}), 200, {'ContentType':'application/json'}
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
 
 
